c75e0700612a8ab1fd578bfe0e6ea75fd1f8f994
rgoshen <rick.goshen@gmail.com>
2022-01-17 14:00:30 -0700

    add drink menu and items

--- snack-or-booze/src/Api.js
+++ snack-or-booze/src/Api.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_API_URL = "http://localhost:5000";
+const BASE_API_URL = 'http://localhost:5000';
 
 /* 
   json-server will give you CRUD endpoints on snacks and drinks.
@@ -10,12 +10,15 @@ const BASE_API_URL = "http://localhost:5000";
 */
 
 class SnackOrBoozeApi {
-
   static async getSnacks() {
     const result = await axios.get(`${BASE_API_URL}/snacks`);
     return result.data;
   }
 
+  static async getDrinks() {
+    const result = await axios.get(`${BASE_API_URL}/drinks`);
+    return result.data;
+  }
 }
 
 export default SnackOrBoozeApi;
--- snack-or-booze/src/App.js
+++ snack-or-booze/src/App.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
-import Home from "./Home";
-import SnackOrBoozeApi from "./Api";
-import NavBar from "./NavBar";
-import { Route, Switch } from "react-router-dom";
-import Menu from "./FoodMenu";
-import Snack from "./FoodItem";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import SnackOrBoozeApi from './Api';
+import NavBar from './NavBar';
+import { Route, Switch } from 'react-router-dom';
+import FoodMenu from './FoodMenu';
+import Snack from './FoodItem';
+import DrinkMenu from './DrinkMenu';
+import Drink from './DrinkItem';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [snacks, setSnacks] = useState([]);
+  const [drinks, setDrinks] = useState([]);
 
   useEffect(() => {
     async function getSnacks() {
@@ -21,24 +24,39 @@ function App() {
     getSnacks();
   }, []);
 
+  useEffect(() => {
+    async function getDrinks() {
+      let drinks = await SnackOrBoozeApi.getDrinks();
+      setDrinks(drinks);
+      setIsLoading(false);
+    }
+    getDrinks();
+  }, []);
+
   if (isLoading) {
     return <p>Loading &hellip;</p>;
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <NavBar />
         <main>
           <Switch>
-            <Route exact path="/">
-              <Home snacks={snacks} />
+            <Route exact path='/'>
+              <Home snacks={snacks} drinks={drinks} />
             </Route>
-            <Route exact path="/snacks">
-              <Menu snacks={snacks} title="Snacks" />
+            <Route exact path='/snacks'>
+              <FoodMenu snacks={snacks} title='Snacks' />
             </Route>
-            <Route path="/snacks/:id">
-              <Snack items={snacks} cantFind="/snacks" />
+            <Route path='/snacks/:id'>
+              <Snack items={snacks} cantFind='/snacks' />
+            </Route>
+            <Route exact path='/drinks'>
+              <DrinkMenu drinks={drinks} title='Drinks' />
+            </Route>
+            <Route path='/drinks/:id'>
+              <Drink items={drinks} cantFind='/drinks' />
             </Route>
             <Route>
               <p>Hmmm. I can't seem to find what you want.</p>
