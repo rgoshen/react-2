8a7303cbdb2a5369f9047ce49152dea30789145a
rgoshen <rick.goshen@gmail.com>
2022-01-17 14:09:22 -0700

    fix heading warning

--- snack-or-booze/src/Home.js
+++ snack-or-booze/src/Home.js
@@ -1,15 +1,13 @@
-import React from "react";
-import { Card, CardBody, CardTitle } from "reactstrap";
+import React from 'react';
+import { Card, CardBody, CardTitle } from 'reactstrap';
 
 function Home() {
   return (
-    <section className="col-md-8">
+    <section className='col-md-8'>
       <Card>
-        <CardBody className="text-center">
-          <CardTitle>
-            <h3 className="font-weight-bold">
-              Welcome to Silicon Valley's premier dive cafe!
-            </h3>
+        <CardBody className='text-center'>
+          <CardTitle tag='h3'>
+            Welcome to Silicon Valley's premier dive cafe!
           </CardTitle>
         </CardBody>
       </Card>
