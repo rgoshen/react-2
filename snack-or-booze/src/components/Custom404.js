56bead56a961531ce124474c5698450f86099044
rgoshen <rick.goshen@gmail.com>
2022-01-17 16:46:55 -0700

    made 404page bigger

--- snack-or-booze/src/components/Custom404.js
+++ snack-or-booze/src/components/Custom404.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NotFoundImage from '../assets/images/404_image.jpeg';
 import '../styles/Custom404.css';
 import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
 function NotFoundPage() {
   return (
-    <section className='col-md-4'>
-      <Card>
-        <CardBody>
-          <CardTitle className='font-weight-bold text-center'>
-            Hmmm. I can't seem to find what you want.
-          </CardTitle>
-        </CardBody>
-        <CardImg alt='Page not found' bottom src={NotFoundImage} width='100%' />
-      </Card>
+    <section className='col-sm-6'>
+      <Link to='/'>
+        <Card>
+          <CardBody>
+            <CardTitle className='font-weight-bold text-center'>
+              Hmmm. I can't seem to find what you want.
+            </CardTitle>
+          </CardBody>
+          <CardImg
+            alt='Page not found'
+            bottom
+            src={NotFoundImage}
+            width='100%'
+          />
+        </Card>
+      </Link>
     </section>
   );
 }
